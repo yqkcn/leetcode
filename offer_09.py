13cd6980a511ab912df13a4049ddc162d3eab55a
yqkcn <410728991@qq.com>
2020-06-30 19:07:05 +0800

    offer_09

---
+++ offer_09.py
@@ -0,0 +1,21 @@
+class CQueue:
+
+    def __init__(self):
+        self._list = []
+
+
+    def appendTail(self, value: int) -> None:
+        self._list.append(value)
+
+
+    def deleteHead(self) -> int:
+        if not self._list:
+            return -1
+        return self._list.pop(0)
+
+
+
+# Your CQueue object will be instantiated and called as such:
+# obj = CQueue()
+# obj.appendTail(value)
+# param_2 = obj.deleteHead()
