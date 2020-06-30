5de702148c31e440b0c57443c4975ceedd850df1
yqkcn <410728991@qq.com>
2020-06-30 22:51:36 +0800

    107

---
+++ 107.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        res, nodes = [], [root]
+        while nodes:
+            values, next_nodes = [], []
+            for node in nodes:
+                values.append(node.val)
+                if node.left:
+                    next_nodes.append(node.left)
+                if node.right:
+                    next_nodes.append(node.right)
+            res.append(values)
+            nodes = next_nodes
+        return list(reversed(res))
