2ad79fef692bc455cea49111e4a553bc81b8c43a
yqkcn <410728991@qq.com>
2020-07-01 10:57:18 +0800

    110

---
+++ 110.py
@@ -0,0 +1,14 @@
+class Solution:
+    # Compute the tree's height via recursion
+    def height(self, root: TreeNode) -> int:
+        if not root:
+            return -1
+        return 1 + max(self.height(root.left), self.height(root.right))
+
+    def isBalanced(self, root: TreeNode) -> bool:
+        # An empty tree satisfies the definition of a balanced tree
+        if not root:
+            return True
+        return abs(self.height(root.left) - self.height(root.right)) < 2 \
+               and self.isBalanced(root.left) \
+               and self.isBalanced(root.right)
