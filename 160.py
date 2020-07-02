715fa5d059d99bbcd86e1f789e1564189c6c297a
yqkcn <410728991@qq.com>
2020-07-02 13:16:11 +0800

    160

---
+++ 160.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        while headA:
+            nodeB = headB
+            while nodeB:
+                if nodeB == headA:
+                    return nodeB
+                nodeB = nodeB.next
+            headA = headA.next
