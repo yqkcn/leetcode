cd671ea929a548127e4602a288cee7c0ebba48b8
yqkcn <410728991@qq.com>
2020-07-18 01:30:54 +0800

    98

---
+++ 98.go
@@ -0,0 +1,26 @@
+package main
+
+import "math"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isValidBST(root *TreeNode) bool {
+	return isValid(root, math.MinInt64, math.MaxInt64)
+}
+
+func isValid(root *TreeNode, lower int, upper int) bool {
+	if root == nil {
+		return true
+	}
+	if root.Val <= lower || root.Val >= upper {
+		return false
+	}
+	return isValid(root.Left, lower, root.Val) && isValid(root.Right, root.Val, upper)
+}
