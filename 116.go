22154dd9d8e8af882f4d492ccdfcd5bf0d8bf8bc
yqkcn <410728991@qq.com>
2020-07-18 10:08:04 +0800

    116

---
+++ 116.go
@@ -0,0 +1,27 @@
+package main
+
+
+Definition for a Node.
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil || root.Left == nil {
+		return root
+	}
+	markNode(root)
+	return root
+}
+
+func markNode(node *Node) {
+	node.Left.Next = node.Right
+	if node.Next != nil {
+		node.Right.Next = node.Next.Left
+	}
+	connect(node.Left)
+	connect(node.Right)
+}
