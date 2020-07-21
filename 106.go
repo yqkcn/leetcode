3b97f97c1111f57be0612ae821bf55985ffde15c
yqkcn <410728991@qq.com>
2020-07-21 23:29:48 +0800

    106

---
+++ 106.go
@@ -0,0 +1,25 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	if len(postorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{postorder[len(postorder)-1], nil, nil}
+	i := 0
+	for ; i < len(inorder); i++ {
+		if inorder[i] == root.Val {
+			break
+		}
+	}
+	root.Left = buildTree(inorder[:i], postorder[:i])
+	root.Right = buildTree(inorder[i+1:], postorder[i:len(postorder)-1])
+	return root
+}
