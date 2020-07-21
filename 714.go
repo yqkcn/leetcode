39c219afb6f7d51536a8ba40de3428f1cb83f724
yqkcn <410728991@qq.com>
2020-07-21 13:04:57 +0800

    714

---
+++ 714.go
@@ -0,0 +1,25 @@
+package main
+
+import "math"
+
+func maxProfit(prices []int, fee int) int {
+	n := len(prices)
+	if n == 0 {
+		return 0
+	}
+	dq_0, dq_1 := 0, math.MinInt64
+	for i := 0; i < n; i++ {
+		tmp := dq_0
+		dq_0 = max(dq_0, dq_1+prices[i])
+		dq_1 = max(dq_1, tmp-prices[i]-fee)
+	}
+	// 0肯定比1利润大
+	return dq_0
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
