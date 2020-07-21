766229ace469bd4c3801e6d85348304d475417a6
yqkcn <410728991@qq.com>
2020-07-21 18:59:24 +0800

    124

---
+++ 124.go
@@ -0,0 +1,36 @@
+package main
+
+import "math"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxPathSum(root *TreeNode) int {
+	maxSum := math.MinInt32
+	var maxGain func(*TreeNode) int
+	maxGain = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		left := max(maxGain(node.Left), 0)
+		right := max(maxGain(node.Right), 0)
+		price := node.Val + left + right
+		maxSum = max(maxSum, price)
+
+		return node.Val + max(left, right)
+	}
+	maxGain(root)
+	return maxSum
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
