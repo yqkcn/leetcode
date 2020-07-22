9d71887b7f22530b56f7afc6ff80c1e08e536483
yqkcn <410728991@qq.com>
2020-07-22 10:12:34 +0800

    240

---
+++ 240.go
@@ -0,0 +1,22 @@
+package main
+
+func searchMatrix(matrix [][]int, target int) bool {
+	if matrix == nil || len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+	n, m := len(matrix), len(matrix[0])
+	for i := 0; i < n; i++ {
+		if target > matrix[i][m-1] {
+			continue
+		}
+		if target < matrix[i][0] {
+			return false
+		}
+		for j := 0; j < m; j++ {
+			if matrix[i][j] == target {
+				return true
+			}
+		}
+	}
+	return false
+}
