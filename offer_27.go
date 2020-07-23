07e7e8c0514db9e7e9286426707d53909ea4d448
yqkcn <410728991@qq.com>
2020-07-23 13:25:16 +0800

    offer 27

---
+++ offer_27.go
@@ -0,0 +1,29 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	queue := []*TreeNode{root}
+	for len(queue) != 0 {
+		node := queue[len(queue)-1]
+		queue = queue[:len(queue)-1]
+		// 翻转
+		node.Left, node.Right = node.Right, node.Left
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+	}
+	return root
+}
