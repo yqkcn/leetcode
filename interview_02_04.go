7c775533f8dbd6a1b7a706ba3f02103bd321fc2c
yqkcn <410728991@qq.com>
2020-07-27 13:18:31 +0800

    interview 02 04

---
+++ interview_02_04.go
@@ -0,0 +1,30 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func partition(head *ListNode, x int) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	before := &ListNode{}
+	after := &ListNode{}
+	before1, after1 := before, after
+	for head != nil {
+		if head.Val < x {
+			before.Next = head
+			before = before.Next
+		} else {
+			after.Next = head
+			after = after.Next
+		}
+		head = head.Next
+	}
+	after.Next = nil
+	before.Next = after1.Next
+	return before1.Next
+}
