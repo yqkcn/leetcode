8540ca427fa04fa2d8f711ee7fd71b23d2c1f153
yqkcn <410728991@qq.com>
2020-08-04 21:34:41 +0800

    530

---
+++ 530.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"math"
+	"sort"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func getMinimumDifference(root *TreeNode) int {
+	values := getValues(root)
+	sort.Ints(values)
+	res := math.MaxInt64
+	for i := 0; i < len(values)-1; i++ {
+		if values[i+1]-values[i] < res {
+			res = values[i+1] - values[i]
+		}
+	}
+	return res
+}
+
+func getValues(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	res := []int{root.Val}
+	res = append(res, getValues(root.Left)...)
+	res = append(res, getValues(root.Right)...)
+	return res
+}
