65e6a310067c7b81fb164f6d9b8b160a5ae931e8
yqkcn <410728991@qq.com>
2021-04-19 13:18:29 +0800

    format

--- bracket-lcci.py
+++ bracket-lcci.py
@@ -5,18 +5,18 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = set()
 
-        def dfs(l: int, r: int, ln: int, char: str):
-            if l < 0 or r < 0 or ln < 0:
+        def dfs(l: int, r: int, char: str):
+            if l < 0 or r < 0 or l > r:
                 return
-            if l == 0 and r == 1 and ln == 1:
+            if l == 0 and r == 1:
                 char += ")"
                 if char not in res:
                     res.add(char)
                 return
-            dfs(l - 1, r, ln + 1, char + "(")
-            dfs(l, r - 1, ln - 1, char + ")")
+            dfs(l - 1, r, char + "(")
+            dfs(l, r - 1, char + ")")
 
-        dfs(n, n, 0, "")
+        dfs(n, n, "")
         return list(res)
 
 
