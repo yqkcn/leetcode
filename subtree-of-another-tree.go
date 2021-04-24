01b0ce4f1481f0433543b93dfb6fc73782868a5d
yqkcn <410728991@qq.com>
2021-04-24 17:46:42 +0800

    subtree-of-another-tree

---
+++ subtree-of-another-tree.go
@@ -0,0 +1,30 @@
+package main
+
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSubtree(s *TreeNode, t *TreeNode) bool {
+	if s == nil {
+		return false
+	}
+	return check(s, t) || isSubtree(s.Left, t) || isSubtree(s.Right, t)
+}
+
+func check(a, b *TreeNode) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	if a.Val == b.Val {
+		return check(a.Left, b.Left) && check(a.Right, b.Right)
+	}
+	return false
+}
