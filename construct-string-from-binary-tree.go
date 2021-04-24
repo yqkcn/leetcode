53ced3fe8b63a7d5d2a3c97f6ee4392aa67645b2
yqkcn <410728991@qq.com>
2021-04-24 18:06:33 +0800

    construct-string-from-binary-tree

---
+++ construct-string-from-binary-tree.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"strconv"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func tree2str(t *TreeNode) string {
+	if t == nil {
+		return ""
+	}
+	if t.Left == nil && t.Right == nil {
+		return strconv.Itoa(t.Val)
+	}
+	if t.Left != nil && t.Right == nil {
+		return strconv.Itoa(t.Val) + "(" + tree2str(t.Left) + ")"
+	}
+	if t.Left == nil && t.Right != nil {
+		return strconv.Itoa(t.Val) + "()" + "(" + tree2str(t.Right) + ")"
+	}
+	return strconv.Itoa(t.Val) + "(" + tree2str(t.Left) + ")" + "(" + tree2str(t.Right) + ")"
+}
