64c499737918a28d0285b6d885d5d9b779f10678
yqkcn <410728991@qq.com>
2021-07-29 18:59:51 +0800

    minimum-operations-to-make-the-array-increasing

---
+++ minimum-operations-to-make-the-array-increasing.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        answer = 0
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i-1]:
+                answer += nums[i-1] + 1 - nums[i]
+                nums[i] = nums[i-1] + 1
+        return answer
