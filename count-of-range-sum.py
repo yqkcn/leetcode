75abef88c418ee2cbb364c33b0b7bac8935dd575
yqkcn <410728991@qq.com>
2021-08-03 12:24:41 +0800

    right

--- count-of-range-sum.py
+++ count-of-range-sum.py
@@ -25,5 +25,14 @@ class Solution:
         return answer
 
 
+class Solution:
+    def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
+        res, pre, now = 0, [0], 0
+        for n in nums:
+            now += n
+            res += bisect.bisect_right(pre, now - lower) - bisect.bisect_left(pre, now - upper)
+            bisect.insort(pre, now)
+        return res
+
 if __name__ == '__main__':
     print(Solution().countRangeSum(nums=[-2, 5, -1], lower=-2, upper=2))
