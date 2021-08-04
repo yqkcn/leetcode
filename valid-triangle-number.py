85e646e80054c08f09654a0a3608514d45748730
yqkcn <410728991@qq.com>
2021-08-04 11:28:23 +0800

    bisect

--- valid-triangle-number.py
+++ valid-triangle-number.py
@@ -8,15 +8,14 @@ class Solution:
         nums.sort()
         for i in range(n):
             for j in range(i + 1, n):
-                left, right, k = j + 1, n - 1, j
+                left, right = j + 1, n - 1
                 while left <= right:
                     mid = (left + right) // 2
                     if nums[mid] < nums[i] + nums[j]:
-                        k = mid
                         left = mid + 1
                     else:
                         right = mid - 1
-                answer += k - j
+                answer += left - 1 - j
         return answer
 
 
